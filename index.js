cda26e0b857e97b6546fab885e6db961eeb6969f
jden <jason@denizac.org>
2014-07-27 16:01:56 -0700

    foo

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function ttx () {
-  
-}
+process.stdin.setRawMode(true)
+var keypress = require('keypress')
+keypress(process.stdin)
 
-module.exports = ttx
+process.stdin.on('keypress', function (chunk, key) {
+  // process.stdout.write('k' + key)
+  console.log(chunk, key)
+  // console.log(new Buffer(key.sequence), key.sequence)
+  if (key && key.ctrl && key.name == 'd') { process.exit()}
+})
+
+setTimeout(function () {
+
+}, 1000000)
